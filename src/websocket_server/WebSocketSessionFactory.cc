50ef46436fc1a3ac44ac2ac4f4cba2b629114131
Rachid Mzannar <r.mzannar@ostfalia.de>
2020-10-30 18:54:25 +0100

    add WebSocketSessionFactory

---
+++ src/websocket_server/WebSocketSessionFactory.cc
@@ -0,0 +1,17 @@
+#include "websocket_server/WebSocketSessionFactory.hh"
+#include "websocket_server/PlainWebSocketSession.hh"
+#include "websocket_server/SSLWebSocketSession.hh"
+
+using namespace amadeus;
+
+std::shared_ptr<PlainWebSocketSession>
+make_websocket_session(beast::tcp_stream _stream)
+{
+    return std::make_shared<PlainWebSocketSession>(std::move(_stream));
+}
+
+std::shared_ptr<SSLWebSocketSession>
+make_websocket_session(beast::ssl_stream<beast::tcp_stream> _stream)
+{
+    return std::make_shared<SSLWebSocketSession>(std::move(_stream));
+}
--- src/websocket_server/WebSocketSessionFactory.hh
+++ src/websocket_server/WebSocketSessionFactory.hh
@@ -1,21 +1,24 @@
 #ifndef WEBSOCKET_SERVER_WEBSOCKET_SESSION_FACTORY_HH
 #define WEBSOCKET_SERVER_WEBSOCKET_SESSION_FACTORY_HH
 
-#include "websocket_server/PlainWebSocketSession.hh"
-#include "websocket_server/SSLWebSocketSession.hh"
+#include "websocket_server/asiofwd.hh"
+
+#include <boost/beast/core/tcp_stream.hpp>
+#include <boost/beast/ssl/ssl_stream.hpp>
+
+#include <memory>
 
 namespace amadeus {
-std::shared_ptr<PlainWebSocketSession>
-make_websocket_session(beast::tcp_stream _stream)
-{
-    return std::make_shared<PlainWebSocketSession>(std::move(_stream));
-}
+/// Forward declarations
+class PlainWebSocketSession;
+class SSLWebSocketSession;
 
-std::shared_ptr<SSLWebSocketSession>
-make_websocket_session(beast::ssl_stream<beast::tcp_stream> _stream)
-{
-    return std::make_shared<SSLWebSocketSession>(std::move(_stream));
-}
+inline static std::shared_ptr<PlainWebSocketSession>
+make_websocket_session(beast::tcp_stream _stream);
+
+inline static std::shared_ptr<SSLWebSocketSession>
+make_websocket_session(beast::ssl_stream<beast::tcp_stream> _stream);
+
 } // namespace amadeus
 
 #endif // !WEBSOCKET_SERVER_WEBSOCKET_SESSION_FACTORY_HH
