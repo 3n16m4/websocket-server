f5f02c84ba0027c1bce73416e889d968c45dd919
Rachid Mzannar <r.mzannar@ostfalia.de>
2020-11-09 21:55:56 +0100

    refactor CLI class to extra module

---
+++ src/websocket_server/CommandLineInterface.cc
@@ -0,0 +1,31 @@
+#include "websocket_server/CommandLineInterface.hh"
+
+#include <iostream>
+#include <stdexcept>
+#include <thread>
+
+using namespace amadeus;
+
+void CommandLineInterface::parse(int _argc, char* _argv[])
+{
+    // ./websocket_server <address> <port> <docroot> <threads>
+    if (_argc != 5) {
+        throw std::invalid_argument(
+            "Usage: ./websocket_server <address> "
+            "<port> <threads>.\nExample: ./websocket_server 127.0.0.1 8080 "
+            ". "
+            "8\nYou may also set <threads> to 0 to enable full utilization "
+            "of the hardware threads.");
+    }
+    ip = asio::ip::make_address(_argv[1]);
+    port = static_cast<decltype(port)>(std::atoi(_argv[2]));
+    docRoot = _argv[3];
+    threads = static_cast<decltype(threads)>(std::atoi(_argv[3]));
+
+    if (threads == 0) {
+        threads = std::thread::hardware_concurrency();
+    }
+
+    std::cout << "Arguments: " << ip << " " << port << " " << docRoot << " "
+              << threads << '\n';
+}
---
+++ src/websocket_server/CommandLineInterface.hh
@@ -0,0 +1,28 @@
+#ifndef WEBSOCKET_SERVER_COMMAND_LINE_INTERFACE_HH
+#define WEBSOCKET_SERVER_COMMAND_LINE_INTERFACE_HH
+
+#include "websocket_server/asiofwd.hh"
+
+#include <boost/asio/ip/address.hpp>
+
+#include <cstdint>
+#include <string>
+
+namespace amadeus {
+/// \brief A simple CLI for parsing the necessary options for the TCP Listener.
+struct CommandLineInterface
+{
+    /// The IP Address the server will listen to.
+    asio::ip::address ip;
+    /// The Port the server will listen to.
+    std::uint16_t port;
+    /// The document root of all the resources the server will serve.
+    std::string docRoot;
+    /// The amount of threads the server will utilize.
+    std::uint32_t threads;
+
+    void parse(int _argc, char* _argv[]);
+};
+} // namespace amadeus
+
+#endif // !WEBSOCKET_SERVER_COMMAND_LINE_INTERFACE_HH
