142c87ab0d335360395ce80905df037e14d8c2e9
Rachid Mzannar <r.mzannar@ostfalia.de>
2020-11-18 18:48:08 +0100

    update build instructions

---
+++ run.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+
+docker build -t webserver:latest .
+docker run -it -p 80:8080 -p 81:8081 webserver:latest
--- run.sh
+++ run.sh
@@ -2,4 +2,3 @@
 
 docker build -t webserver:latest .
 docker run -it -p 80:8080 -p 81:8081 webserver:latest
-
